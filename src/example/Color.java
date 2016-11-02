d68ebc3bac974a52cc701f1aaa462a9c685df3e2
Testehan, Dan <dan.testehan@accenture.com>
2016-11-02 14:02:45 +0200

    added example for Abstract factory pattern

---
+++ src/example/Color.java
@@ -0,0 +1,5 @@
+package example;
+
+public interface Color {
+	void fill();
+}
---
+++ src/example/ColorFactory.java
@@ -0,0 +1,23 @@
+package example;
+
+public class ColorFactory extends AbstractFactory{
+	  
+	@Override
+	public Shape getShape(String shapeType){
+		return null;
+	}
+	   
+	@Override
+	public Color getColor(String color) {
+		if(color == null){
+			return null;
+	    }		
+	      
+	    if(color.equalsIgnoreCase("RED")){
+	    	return new Red();
+	    }else if(color.equalsIgnoreCase("GREEN")){
+	    	return new Green();
+	    }
+	      return null;
+	}
+}
